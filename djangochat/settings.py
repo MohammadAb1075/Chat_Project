893ab673bf127d686ff788e7e78b47b1ada10271
MohammadAb1075 <mabdollah1375@gmail.com>
2019-08-23 20:45:19 +0430

    first commit

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -59,7 +59,7 @@ CELERY_TIMEZONE = "UTC"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = 'mabdollah75'
+EMAIL_HOST_PASSWORD = '*******'
 EMAIL_PORT = 587
 
 
